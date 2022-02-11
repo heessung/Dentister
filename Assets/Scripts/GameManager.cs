55c5f55e0a0a34c9f104dabfedea878305d2a22d
DESKTOP-PTPDIVH\twink <twinklsetarsheep@gmail.com>
2022-02-12 02:44:29 +0900

    초기 커밋.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        
+    }
+}
